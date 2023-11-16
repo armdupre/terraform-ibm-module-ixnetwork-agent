07253dc9b247a687e6e9e41182498bc26b3dc2f5
armdupre <a-joseph.dupre@keysight.com>
2023-11-16 18:47:44 -0500

    Updated.

--- output.tf
+++ output.tf
@@ -1,16 +1,19 @@
 output "Eth0FloatingIp" {
+	description = "Floating Ip resource associated with the first network interface."
 	value = {
 		"address" : "https://${ibm_is_floating_ip.Eth0FloatingIp.address}"
 	}
 }
 
 output "Image" {
+	description = "Custom image resource associated with the virtual server instance."
 	value = {
 		"name" : data.ibm_is_image.Image.name
 	}
 }
 
 output "Instance" {
+	description = "Instance resource associated with the virtual server instance."
 	value = {
 		"eth0_address" : ibm_is_instance.Instance.primary_network_interface[0].primary_ip[0].address
 	}
